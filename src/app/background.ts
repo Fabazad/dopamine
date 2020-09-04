4d87d09e77c492fe101cffde7fe12e10cd1a5708
Fabien <fabien.turgut@gmil.com>
2020-09-04 21:15:31 +0200

    icons and do it through background

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,3 +1,19 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    chrome.tabs.update(sender.tab.id, {url: request.redirect});
-});
+const networkFilters = {
+    urls: ["<all_urls>"]
+};
+
+const tag = "fabazad7403-21";
+const urlRegExp: RegExp = /https?:\/\/www\.amazon\.(com|fr).*\/dp\/([A-Z0-9]{10})/;
+
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+    const {method, url} = details;
+    if (method !== "GET") return;
+
+    const matchProductUrl: RegExpMatchArray | null = url.match(urlRegExp);
+    const matchTag: Boolean = url.includes(`tag=${tag}`);
+
+    if (matchProductUrl && !matchTag) {
+        const asin: string = matchProductUrl[2];
+        chrome.tabs.update({url: `https://www.amazon.fr/dp/${asin}?tag=${tag}`});
+    }
+}, networkFilters);
